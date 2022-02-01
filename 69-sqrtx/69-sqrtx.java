9e63007897c686942922120716042616cab073f5
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-02-01 14:59:22 +0530

    Time: 1 ms (99.90%), Space: 39.7 MB (6.23%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
        if(x==0) return 0;
-        int low=1,high=x/2;
+        int low=1,high=x;
         int ans=1;
         while(low<=high){
             int mid=(low+high)/2;
