8585a14f27f71a56b1942d27d7cbf89f30968aee
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-02-24 00:42:51 +0530

    Time: 4 ms (83.74%), Space: 10.4 MB (60.58%) - LeetHub

---
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        unordered_map<int,vector<int>> m;
+        
+        for(int i=0;i<pieces.size();i++)
+        {
+            m[pieces[i][0]] = pieces[i];
+        }
+        
+        for(int i=0;i<arr.size();)
+        {
+            if(m.find(arr[i])==m.end())
+                return false;
+            
+            vector<int> temp = m[arr[i]];
+            
+            for(int j=0;j<temp.size();j++,i++)
+            {
+                if(arr[i]!=temp[j])
+                    return false;
+            }
+        }
+        
+        return true;
+        
+        
+        
+    }
+};
