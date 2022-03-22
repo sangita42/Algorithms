f0020430e0e8aeaa838eb24cbffdf8b775e03549
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-03-22 20:50:25 +0530

    Time: 31 ms (5.26%), Space: 5.9 MB (80.09%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fibo(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        else{
+            return fibo(n-1)+fibo(n-2);
+        }
+    }
+    int fib(int n) {
+        return fibo(n);
+        
+        
+    }
+};
