03d930138256f9404585e6a9acfc01b3fbdfd932
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-04-17 16:28:22 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (84.06%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,32 +1,45 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-         int[][] grid = new int[m][n];
-    for(int i = 0; i<m; i++){
-        for(int j = 0; j<n; j++){
-             if(obstacleGrid[i][j]==1){
-                grid[i][j]=0;
-            }
-            else if(i==0&&j==0){
-                grid[i][j] = 1;
-            }
-            else if(i==0){
-                grid[i][j]=grid[i][j-1];
-            }
-            else if(j==0){
-                grid[i][j]=grid[i-1][j];
-            }
-            
-            
-            else{
-                
-                grid[i][j]=grid[i][j-1]+grid[i-1][j];
-               
+    // i=2,j=2 fnc(2,2)-> func(1,2)+func(2,1)
+    //                    func(0,2)+func(1,1)
+    
+    int finc(int i,int j,int[][] arr){
+        
+        if(i==0&&j==0 &&arr[i][j]!=1){
+            return 1;
         }
+        if(i<0|j<0||arr[i][j]==1){
+            return 0;
+        }
+        return finc(i-1,j,arr)+finc(i,j-1,arr);
     }
-    }
-    return grid[m-1][n-1];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n=obstacleGrid.length;
+        int m=obstacleGrid[0].length;
+        int[][] dp =new int[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }
+                else if(i==0&&j==0){
+                    dp[i][j]=1;
+                    
+                }
+                 else{
+                     int left=0,right=0;
+                     if(i>0){
+                         left=dp[i-1][j];
+                     }
+                     if(j>0){
+                         right=dp[i][j-1];
+                     }
+                dp[i][j]=left+right;
+                     }
+            }
+        }
+        
+        return dp[n-1][m-1];
         
     }
 }
