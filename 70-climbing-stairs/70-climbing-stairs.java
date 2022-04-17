4ab0c11bff3ddc2e576f934e3604e5005860f271
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-04-17 09:20:58 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (81.24%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -15,7 +15,15 @@ class Solution {
         // func(0,3)-> 2.func(1,3)->1.func(2,3)+1.func(3,3)->  1.func(3,3)+0.func(4,3)
                        //1.func(2,3)->1.func(3,3)+func(4,3)
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-         return climb(0,n,dp);                  
+        //Arrays.fill(dp,-1);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int left=0;
+            if(i>1){
+                left=dp[i-2];
+            }
+            dp[i]=dp[i-1]+left;
+        }
+         return dp[n];                  
     }
 }
