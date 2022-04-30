4b31b18c3406c6445f66fe16c64843af1448c0c7
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-04-30 17:09:12 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (28.78%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -1,21 +1,24 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int i=0, j=0;
+        int n=nums.length;
+        int i=0,j=0;
         int sum=0;
-        int m=Integer.MAX_VALUE;
-        while(j<nums.length){
-            sum+=nums[j];
-            
-                while(sum>=target){
-                    m=Math.min(m,j-i+1);
-                    sum-=nums[i];
-                    i++;
+        int ans=Integer.MAX_VALUE;
+        while(j<n){
+            sum+=nums[j];// sum=6+3=9
+           
+             while(sum>=target){
+                 ans=Math.min(ans,j-i+1);//ans=2
+                    sum-=nums[i];//9-2=7
+                    i++;//i=4
                 }
-            
-            j++;
+            j++;//j=5
             
         }
-        return m==Integer.MAX_VALUE?0:m;
+        if(ans==Integer.MAX_VALUE){
+            return 0;
+        }
+        return ans;
         
     }
 }
