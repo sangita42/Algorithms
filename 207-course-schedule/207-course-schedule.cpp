e5c1792abc30bbeb571bb5086575c332a03e624f
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-01 12:26:31 +0530

    Time: 29 ms (52.64%), Space: 13.1 MB (92.36%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
+    void dfs(int node,vector<int>&vis,stack<int>&st,vector<int>adj[]){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(it,vis,st,adj);
+            }
+        }
+        st.push(node);
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[numCourses];
+        
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        vector<int> indegree(numCourses,0);
+        vector<int>indegree(numCourses,0);
         for(int i=0;i<numCourses;i++){
             for(auto it:adj[i]){
                 indegree[it]++;
@@ -17,19 +27,21 @@ public:
                 q.push(i);
             }
         }
+        
         int c=0;
         while(!q.empty()){
-            int node = q.front();
+            int temp=q.front();
             q.pop();
             c++;
-            for(auto it:adj[node]){
+            for(auto it:adj[temp]){
                 indegree[it]--;
                 if(indegree[it]==0){
                     q.push(it);
                 }
             }
         }
+        if(c==numCourses) return true;
+        return false;
         
-        return (c==numCourses);
     }
 };
