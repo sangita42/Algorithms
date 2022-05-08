db679cce10950c682eca5d4478a533ee6bbcc674
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-08 09:57:58 +0530

    Time: 18 ms (92.81%), Space: 13.6 MB (69.58%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>&vis,stack<int>&st,vector<int>adj[]){
+    bool cycle(int node,vector<int>&vis,vector<int>&dfsvis,vector<int>adj[]){
         vis[node]=1;
+        dfsvis[node]=1;
         for(auto it:adj[node]){
             if(vis[it]==0){
-                dfs(it,vis,st,adj);
+                if(cycle(it,vis,dfsvis,adj)){
+                    return true;
+                }
+            }
+            else if(dfsvis[it]==1){
+                return true;
+                
             }
         }
-        st.push(node);
+        dfsvis[node]=0;
+        return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>adj[numCourses];
@@ -15,33 +23,17 @@ public:
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        vector<int>indegree(numCourses,0);
+        vector<int>vis(numCourses,0);
+        vector<int>dfsvis(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
-        }
-        queue<int>q;
-        for(int i=0;i<indegree.size();i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
-        
-        int c=0;
-        while(!q.empty()){
-            int temp=q.front();
-            q.pop();
-            c++;
-            for(auto it:adj[temp]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+            if(vis[i]==0){
+                if(cycle(i,vis,dfsvis,adj)){
+                    return false;
                 }
             }
         }
-        if(c==numCourses) return true;
-        return false;
+        return true;
+        
         
     }
 };
