4922c1f94578a473190aa2c15a40ee5e5d76a9ea
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-16 21:45:04 +0530

    Time: 15 ms (27.28%), Space: 10.6 MB (60.96%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
-    // 2 3 1 2 4 3 
-    //k = 3 -> 6, 6, 7,9 = 9
-    //k=2 5 4 3 7 
-    // 2 3 1 2 4 3 low=1, high=2 mid=2
-    int maxsubarray(vector<int>& nums,int target,int k){
-        int i=0,j=0;
+    int minSubArrayLen(int target, vector<int>& nums) {
         int n=nums.size();
+        int i=0,j=0;
         int sum=0;
-        int ans=0;
-        while(j<n){
-            sum+=nums[j];// sum= 5
-            if(j-i+1==k){
-               
-                ans=max(ans,sum);//ans=6
-                sum-=nums[i];
-                i++;
+        int ans=INT_MAX;
+        while(j<n){//5
+            sum+=nums[j];//
+            while(sum>=target){
+                 ans=min(ans,j-i+1);
+                sum-=nums[i];//6
+                i++;//3
             }
+           //2
             j++;
-            
-            
+        }
+        if(ans==INT_MAX){
+            return 0;
         }
         return ans;
-        
-    }
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int low=1,high=nums.size()+1;
-        //low=4,high=5
-        bool ans=false;
-        while(low<high){
-            int mid=(low+high)/2;//low=5
-            if(maxsubarray(nums,target,mid)>=target){//
-                ans=true;
-                
-                high=mid;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        if(ans) return low;
-        return 0;
-        
-        
-        
     }
 };
