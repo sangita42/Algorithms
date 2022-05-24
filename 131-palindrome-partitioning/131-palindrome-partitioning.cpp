fce5566878a2132e18321bce6ccb023e323a3608
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-24 08:42:09 +0530

    Time: 182 ms (48.36%), Space: 57.4 MB (69.64%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool iPalindrome(string s,int start,int end){
+    bool palindrome(string s, int start, int end){
         while(start<=end){
             if(s[start]!=s[end]){
                 return false;
@@ -9,33 +9,26 @@ public:
             end--;
         }
         return true;
-        
     }
-    // aab
-    //func()
-    //for(int i=ind;)
-    //
-    void func(int ind,string s, vector<string>&ans,vector<vector<string>>&res){
-        
+    void pal(int ind, string s,vector<string>&ans,vector<vector<string>>&res ){
         if(ind==s.length()){
             res.push_back(ans);
             return;
         }
         for(int i=ind;i<s.length();i++){
-            if(iPalindrome(s,ind,i)){
+            if(palindrome(s,ind, i)){
                 ans.push_back(s.substr(ind,i-ind+1));
-                func(i+1,s,ans,res);
+                pal(i+1,s,ans,res);
                 ans.pop_back();
+                
             }
-            
         }
     }
     vector<vector<string>> partition(string s) {
         vector<string>ans;
         vector<vector<string>>res;
-        func(0,s,ans,res);
+        pal(0,s,ans,res);
         return res;
-        
         
     }
 };
