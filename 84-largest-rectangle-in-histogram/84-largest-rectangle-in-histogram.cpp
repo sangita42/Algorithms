8a1efd5d303a0d378d2f5f6d28a34acaa67e0dce
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-26 20:54:54 +0530

    Time: 409 ms (6.57%), Space: 91.7 MB (15.13%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -10,14 +10,14 @@ public:
             if(st1.empty()){
                 left.push_back(-1);
             }
-            else if(st1.size()>0&&heights[st1.top()]<heights[i]){
+            else if(st1.size()>0&&heights[i]>heights[st1.top()]){
                 left.push_back(st1.top());
             }
-            else if(st1.size()>0&&heights[st1.top()]>=heights[i]){
-                while(st1.size()>0&&heights[st1.top()]>=heights[i]){
+            else if(st1.size()>0&&heights[i]<=heights[st1.top()]){
+                while(st1.size()>0&&heights[i]<=heights[st1.top()]){
                     st1.pop();
                 }
-                if(st1.size()==0){
+                if(st1.empty()){
                     left.push_back(-1);
                 }
                 else{
@@ -25,21 +25,19 @@ public:
                 }
             }
             st1.push(i);
-            
-            
         }
-         for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             if(st2.empty()){
                 right.push_back(n);
             }
-            else if(st2.size()>0&&heights[st2.top()]<heights[i]){
+            else if(st2.size()>0&&heights[i]>heights[st2.top()]){
                 right.push_back(st2.top());
             }
-            else if(st2.size()>0&&heights[st2.top()]>=heights[i]){
-                while(st2.size()>0&&heights[st2.top()]>=heights[i]){
+            else if(st2.size()>0&&heights[i]<=heights[st2.top()]){
+                while(st2.size()>0&&heights[i]<=heights[st2.top()]){
                     st2.pop();
                 }
-                if(st2.size()==0){
+                if(st2.empty()){
                     right.push_back(n);
                 }
                 else{
@@ -47,15 +45,12 @@ public:
                 }
             }
             st2.push(i);
-            
-            
         }
         reverse(right.begin(),right.end());
         int ans=0;
         for(int i=0;i<n;i++){
             int diff=(right[i]-left[i])-1;
-           
-            ans=max(ans,heights[i]*diff);
+            ans=max(ans, heights[i]*diff);
         }
         return ans;
         
