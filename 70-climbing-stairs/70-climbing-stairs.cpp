0c65060bfce25ff87d0823b7ba1de220ee331bbe
Sangita Dey <33152048+sangita42@users.noreply.github.com>
2022-05-26 19:16:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.53%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -19,10 +19,9 @@ public:
         
     }
     int climbStairs(int n) {
-        //vector<int>dp(n+1,-1);
-        int dp[n+1];
+       int dp[n+1];
         dp[0]=1;
-        dp[1]=1;
+        dp[1]=1;// dp[2]=2 dp[3]=3
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
